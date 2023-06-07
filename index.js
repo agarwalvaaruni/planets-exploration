050ad97e90681b0a288ab679139716d1dc6f0c33
vaaruniagarwal <vaaruni.agarwal@iesoftek.com>
2023-06-07 22:32:06 +0530

    Added my Exploration Data

--- index.js
+++ index.js
@@ -1,61 +1,35 @@
-const http = require('http');
+const { parse } = require('csv-parse'); //to deal with Stream of data
+const fs = require('fs');  // to process CSV file as a Stream
 
-const PORT = 3000;
+const habitablePlanets = [];
 
-const server = http.createServer();
+function isHabitable(data)
+{
+    return data['koi_disposition'] === "CONFIRMED" 
+    && data['koi_insol']>0.36 && data['koi_insol']<1.11 //Temperature
+    && data['koi_prad']<1.6;    //Planetary Rasius
+}
 
-const friends = [
-  {
-    id: 0,
-    name: 'Nikola Tesla',
-  },
-  {
-    id: 1,
-    name: 'Sir Isaac Newton',
-  },
-  {
-    id: 2,
-    name: 'Albert Einstein',
-  }
-];
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))      /*  combining the input data with parse and outputting it as a result
+                            of both the pipe parse
+                            and fs.createReadStream  */
+    .on('data', (data) =>{
+        if(isHabitable(data))
+        {
+        habitablePlanets.push(data);
+        }
+    })
+    .on('end', ()=>{
+        console.log(`${habitablePlanets.length} other planets are just like Earth!\nThey are`);
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        }));
 
-server.on('request', (req, res) => {
-  const items = req.url.split('/');
-  // /friends/2 => ['', 'friends', '2']
-  // /friends/
-  if (req.method === 'POST' && items[1] === 'friends') {
-    req.on('data', (data) => {
-      const friend = data.toString();
-      console.log('Request:', friend);
-      friends.push(JSON.parse(friend));
+    })
+    .on('error',(err)=>{
+        console.log(err);
     });
-    req.pipe(res);
-  } else if (req.method === 'GET' && items[1] === 'friends') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    if (items.length === 3) {
-      const friendIndex = Number(items[2]);
-      res.end(JSON.stringify(friends[friendIndex]));
-    } else {
-      res.end(JSON.stringify(friends));
-    }
-  } else if (req.method === 'GET' && items[1] === 'messages') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<body>');
-    res.write('<ul>');
-    res.write('<li>Hello Isaac!</li>');
-    res.write('<li>What are your thoughts on astronomy?</li>');
-    res.write('</ul>');
-    res.write('</body>');
-    res.write('</html>');
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-}); //127.0.0.1 => localhost
